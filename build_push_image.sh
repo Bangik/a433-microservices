d6da1e3b31ffadb277b595e0944fbe6dccbe0d2d
Bangik <aldi.novaldi.frmnsyah@gmail.com>
2022-11-17 07:50:41 +0700

    update bash

--- build_push_image.sh
+++ build_push_image.sh
@@ -10,7 +10,7 @@ docker images
 docker tag item-app:v1 ghcr.io/bangik/a433-microservices/item-app:v1
 
 # login to github package
-echo ${{ secrets.GITHUB_TOKEN }} | docker login ghcr.io -u bangik --password-stdin
+echo $TOKEN_GITHUB | docker login ghcr.io -u bangik --password-stdin
 
 # push image to github package
 docker push ghcr.io/bangik/a433-microservices/item-app:v1
